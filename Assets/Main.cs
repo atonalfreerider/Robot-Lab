f707b5b2d4a238f760e27511a264b9a5dc06c572
john <john@primitive.io>
2024-04-27 19:57:17 -0700

    cad-in-unity ftw
    https://github.com/i2e-haw-hamburg/cad-in-unity

---
+++ Assets/Main.cs
@@ -0,0 +1,28 @@
+﻿using BasicLoader;
+using STPConverter;
+using STPLoader;
+using STPLoader.Implementation.Model;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    public string stpPath;
+
+    void Awake()
+    {
+        ILoader loader = LoaderFactory.CreateFileLoader(stpPath);
+        IParser parser = ParserFactory.Create();
+        IModel model = parser.Parse(loader);
+
+        if (model is StpFile stpFile)
+        {
+            IConverter converterFactory = ConverterFactory.Create();
+            MeshModel meshModel = converterFactory.Convert(stpFile);
+
+            foreach (AForge.Math.Vector3 vector3 in meshModel.Points)
+            {
+                
+            }
+        }
+    }
+}
